9d2c2949390098c41c5189c16c8be411c330b6e0
Tristan Angieri <tristan.angieri@gmail.com>
2021-03-26 16:47:43 +0000

    added port as constk

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 })
